081a588ef8c6479b12fd2f29787800a1a57963a0
Vaishali-Tapaswi <52520564+Vaishali-Tapaswi@users.noreply.github.com>
2024-08-10 14:39:47 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ class Hello
 {
     public static void main(String []args)
     {
-        System.out.println("My First Java Program at 14.30");
+        System.out.println("My First Java Program at 14.40");
     }
 };
